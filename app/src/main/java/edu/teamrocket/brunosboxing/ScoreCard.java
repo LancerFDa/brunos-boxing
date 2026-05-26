90aa98896c88003a41a537fe8c58c575f525c28b
LancerFDa <rodrgolorenzo@gmail.com>
2026-05-26 18:00:39 +0200

    feat(ScoreCard): Creacion de la clase ScoreCard y constructores

---
+++ app/src/main/java/edu/teamrocket/brunosboxing/ScoreCard.java
@@ -0,0 +1,18 @@
+package edu.teamrocket.brunosboxing;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ScoreCard {
+    
+    String color;
+    String redCorner = "";
+    String blueCorner = "";
+    String[] judgeScoreCard;
+
+    List<Round> round = new ArrayList<Round>();
+
+    ScoreCard(String color){
+        this.color = color;
+    }
+}
