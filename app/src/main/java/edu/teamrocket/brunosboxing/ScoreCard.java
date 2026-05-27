0abfa5b187e1be936db15bca2e3c0e40b5316882
LancerFDa <rodrgolorenzo@gmail.com>
2026-05-27 18:02:25 +0200

    feat(ScoreCard): Añadir loadJudgeScoreCard

--- app/src/main/java/edu/teamrocket/brunosboxing/ScoreCard.java
+++ app/src/main/java/edu/teamrocket/brunosboxing/ScoreCard.java
@@ -1,6 +1,7 @@
 package edu.teamrocket.brunosboxing;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.ArrayList;
 
 public class ScoreCard {
@@ -22,5 +23,22 @@ public class ScoreCard {
 
     void setBCorner(String blueCorner) {
         this.blueCorner = blueCorner;
+    }
+
+    void setJudgeScoreCard(String[] scoreCard){
+        this.judgeScoreCard = scoreCard;
+    }
+
+    void addRound(Round round) {
+        this.round.add(round);
+    }
+
+    public void loadJudgeScoreCard(String[] judgeScoreCard){
+        this.setJudgeScoreCard(judgeScoreCard);
+        
+        for(var roundScore : this.judgeScoreCard){
+            var round = Optional.ofNullable(RoundFactory.getRound(roundScore));
+            round.ifPresent(this::addRound);
+        }
     }
 }
